7b6c68154aaefbb977f5823ce043eaf0d476e7e5
Michael Russo <mrusso>
2017-05-25 22:02:22 -0500

    removed AMP code

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -15,7 +15,6 @@ const App = () => (
     <Helmet title="America's Best Drivers Report" titleTemplate="Allstate %s">
       <html lang="en" amp />
       <meta charSet="utf-8" />
-      <script async src="https://cdn.ampproject.org/v0.js"></script>
       <link rel="canonical" href="https://www.allstate.com/tools-and-resources/americas-best-drivers.aspx" />
       <meta name="viewport" content="width=device-width, minimum-scale=1, initial-scale=1, user-scalable=no"/>
       {/* <meta http-equiv="Pragma" content="no-cache" /> */}
@@ -30,7 +29,6 @@ const App = () => (
       <meta property="og:image:type" content="image/png" />
       <meta property="og:image:width" content="600" />
       <meta property="og:image:height" content="315" />
-      <style amp-boilerplate>{`body{-webkit-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-moz-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-ms-animation:-amp-start 8s steps(1,end) 0s 1 normal both;animation:-amp-start 8s steps(1,end) 0s 1 normal both}@-webkit-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-moz-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-ms-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-o-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}`}</style><noscript>{`<style amp-boilerplate>body{-webkit-animation:none;-moz-animation:none;-ms-animation:none;animation:none}</style>`}</noscript>
       <script src="https://use.fontawesome.com/934103e5b8.js" />
     </Helmet>
     <div styleName="content">
