34f45c65d45dced9a7deae3d0ae0de97cb0041b2
Michael Russo <mrusso>
2017-06-15 10:20:49 -0500

    https://jira.t-3.com/browse/ALLBEST-187

--- src/components/TabNav/TabNav.js
+++ src/components/TabNav/TabNav.js
@@ -18,20 +18,20 @@ const Tab = class Tab extends React.Component {
   render () {
     const {id, title, hasLocations, selectedMap, onTabClick} = this.props
     let additionalClasses = selectedMap === id ? 'selected' : ''
-    let isDisabled
+    let isDisabled = false
     if (!hasLocations) {
       additionalClasses += ' disabled'
       isDisabled = true
     }
 
     return (
-      <li role="tab" styleName={`${_.kebabCase(id)} ${additionalClasses}`} onClickCapture={onTabClick}>
-        <a href="#" onClick={onTabClick} styleName="title">
+      <li role="tab" styleName={`${_.kebabCase(id)} ${additionalClasses}`}>
+        <button type="button" onClick={onTabClick} styleName="title" disabled={isDisabled} aria-disabled={isDisabled}>
           <div>{title}</div>
           <If condition={isDisabled}>
             <div>No data available</div>
           </If>
-        </a>
+        </button>
       </li>
     )
   }
