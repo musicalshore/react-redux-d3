802d92ced743f07f771b1085ba0dd0c430285191
Michael Russo <mrusso>
2017-06-19 02:02:54 -0500

    Added new logo

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -3,12 +3,12 @@ import './style.scss'
 import React from 'react'
 import desktopHeader from './desktop-header.png'
 import mobileLogo from './mobile-logo.png'
+import logo from './logo-large.png'
 
 const alt = `Explore the 2017 Allstate America’s Best Drivers Report. Filter by year or state, then get a list of rankings by Top Cities, Population Density or Rain and Snow. Currently viewing:`
 const Banner = () => (
   <header styleName="container">
-      <img styleName="desktop-and-tablet-header" alt={alt} src={desktopHeader} />
-      <img styleName="mobile-logo" alt={alt} src={mobileLogo} />
+    <img alt={alt} src={logo} />
   </header>
 )
 
