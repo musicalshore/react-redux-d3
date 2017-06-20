1954d1dcc759460f87e6aa4fb946fc706e90dea8
Joshua Brewer <joshua.brewer@t-3.com>
2017-06-20 13:30:30 -0700

    1) To include the legacy content into dist build.

--- webpack/webpack.config.production.js
+++ webpack/webpack.config.production.js
@@ -5,6 +5,7 @@ const DirectoryNamedWebpackPlugin = require('directory-named-webpack-plugin')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
 const StyleLintPlugin = require('stylelint-webpack-plugin')
 const rimraf = require('rimraf')
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   context: path.join(__dirname, '..', 'src'),
@@ -12,7 +13,7 @@ module.exports = {
   output: {
     path: path.join(__dirname, '..', 'dist'),
     filename: '[name].[hash].js',
-    publicPath: '/'
+    publicPath: ''
   },
   node: {
     fs: 'empty',
@@ -34,7 +35,10 @@ module.exports = {
       'environment': '"production"',
       NODE_ENV: JSON.stringify('production')
     }),
-
+    new CopyWebpackPlugin([
+        { from: '../legacy-dev-wrapper' }
+      ],
+      { ignore: [ "index-orig.html" ]}),
     // new webpack.optimize.CommonsChunkPlugin({
     //   name: ['vendor'],
     //   minChunks: 2
@@ -43,7 +47,7 @@ module.exports = {
     //   name: ['manifest']
     // }),
     new (webpack.optimize.UglifyJsPlugin)(),
-    new HtmlWebpackPlugin({template: path.join(__dirname, '..', 'src', 'templates', 'index.ejs')}),
+    new HtmlWebpackPlugin({template: path.join(__dirname, '..', 'src', 'templates', 'full-page-index.ejs')}),
     new webpack.ProvidePlugin({
       $: 'jquery',
       jQuery: 'jquery'
