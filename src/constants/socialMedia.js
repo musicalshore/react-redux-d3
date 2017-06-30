a697b487b13a54ba9ba5ca558aa1551733312b48
Michael Russo <mrusso>
2017-06-30 09:29:13 -0500

    added socialMedia constants

---
+++ src/constants/socialMedia.js
@@ -0,0 +1,5 @@
+export const DEFAULT_SHARE_IMAGE_URL = 'https://image.ibb.co/dzp1v5/ABD_FB_TW_LI.png'
+export const PINTEREST_SHARE_IMAGE_URL = 'https://image.ibb.co/ifNX2k/ABD_Pin.png'
+export const SHARE_URL = 'https://www.allstate.com/tools-and-resources/americas-best-drivers.aspx'
+export const DEFAULT_PAGE_SHARE_COPY = `Is your city home to the best drivers in the US? The 2017 Allstate America's Best Drivers Report® has the answer.`
+export const FACEBOOK_APP_ID = `51244333578`
