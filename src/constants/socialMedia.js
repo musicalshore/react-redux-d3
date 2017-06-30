1d141e083d79d6bd442f3ceed5e0a32a876fb2ac
Michael Russo <mrusso>
2017-06-30 12:49:29 -0500

    added UAT urls for shares

--- src/constants/socialMedia.js
+++ src/constants/socialMedia.js
@@ -1,5 +1,5 @@
-export const DEFAULT_SHARE_IMAGE_URL = 'https://image.ibb.co/dzp1v5/ABD_FB_TW_LI.png'
-export const PINTEREST_SHARE_IMAGE_URL = 'https://image.ibb.co/ifNX2k/ABD_Pin.png'
+export const DEFAULT_SHARE_IMAGE_URL = 'http://allstate-abd.uat.thethinktank.com/2017-delivery-test/img/ABD_FB_TW_LI.png'
+export const PINTEREST_SHARE_IMAGE_URL = 'http://allstate-abd.uat.thethinktank.com/2017-delivery-test/img/ifNX2k/ABD_Pin.png'
 export const SHARE_URL = 'https://www.allstate.com/tools-and-resources/americas-best-drivers.aspx'
 export const DEFAULT_PAGE_SHARE_COPY = `Is your city home to the best drivers in the US? The 2017 Allstate America's Best Drivers Report® has the answer.`
 export const FACEBOOK_APP_ID = `51244333578`
